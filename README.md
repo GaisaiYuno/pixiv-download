6101fdab9a40b9a231ac88596e0cbf8c58c405e4
Steven Meng <steven_mhy@outlook.com>
2020-01-31 11:25:59 +0800

    poi

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # pixiv-download
- A downloader for pixiv
+
+### 简介
+
+随便乱写的pixiv下载器
+
+### 特性
+
+国内用户可以下载（只要你知道id）
+
+### 使用方法
+
+1.把目录里的aria2c.exe拷贝到C:\Windows目录下
+
+2.打开down.py，编辑下载目录地址
+
+3.输入pixiv id
+
+4.等待下载
--- down.py
+++ down.py
@@ -3,7 +3,7 @@ import os,requests
 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
 def init():
     # path=os.getcwd()
-    os.chdir("C:\\Users\\steven_meng\\Desktop\\pixiv_down")
+    os.chdir("C:\\Users\\steven_meng\\Desktop\\pixiv_down") #下载目录地址
     
 if __name__=="__main__":
     init()
@@ -20,9 +20,10 @@ if __name__=="__main__":
         image=data["response"][0]['image_urls']['large']
         print("Downloading "+image)
         image="https://bigimg.cheerfun.dev/get/"+image
-        # command="aria2c "+image+" --conf-path=aria2.conf"+" --user-agent=\""+user_agent+"\""+" --referer=https://www.pixiv.net/artworks/"+id
+        # command="aria2c "+image+" --user-agent=\""+user_agent+"\""+" --referer=https://www.pixiv.net/artworks/"+id
+        command="aria2c "+image+" --user-agent=\""+user_agent+"\""
         # command="aria2c "+image+" --conf-path=aria2.conf"+" --referer=https://www.pixiv.net/artworks/"+id
-        command="saldl "+image+" --referer=https://www.pixiv.net/artworks/"+id+" --skip-TLS-verification --connections=15 --connection-max-rate=0 --chunk-size=4M"
+        # command="saldl "+image+" --referer=https://www.pixiv.net/artworks/"+id+" --skip-TLS-verification --connections=15 --connection-max-rate=0 --chunk-size=4M"
         print("Command is:"+command)
         os.system(command)
         # https://www.pixiv.net/artworks/79154264
