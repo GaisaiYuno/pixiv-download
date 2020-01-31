9f87a2e517f10e78f2ebed97d5e3125ace6b8c34
Steven Meng <steven_mhy@outlook.com>
2020-01-31 15:30:03 +0800

    ?????????????

--- down.py
+++ down.py
@@ -2,12 +2,33 @@ import os,requests
 import configparser
 
 user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36"
+path="C:\\Users\\steven_meng\\Documents\\GitHub\\pixiv-download\\picture"
+
 def init():
     cf=configparser.ConfigParser()
     filename=cf.read("config.ini")
     global mainland
     mainland=cf.get("pixiv","chinese_mainland")
-    os.chdir("C:\\Users\\steven_meng\\Documents\\GitHub\\pixiv-download\\picture")
+    os.chdir(path)
+
+def download(image,id):
+    p=path+"\\"+id
+    if (os.path.isdir(p)==False):
+        os.mkdir(p)
+    os.chdir(p)
+    if (mainland):
+        image="https://bigimg.cheerfun.dev/get/"+image
+        command="aria2c "+image+" --user-agent=\""+user_agent+"\""
+    else:
+        command="aria2c "+image+" --user-agent=\""+user_agent+"\""+" --referer=https://www.pixiv.net/artworks/"+id
+    print("Downloading "+image)
+    print("Command is:"+command)
+    os.system(command)
+
+info=""
+def make_info(data):
+    pass
+
 if __name__=="__main__":
     init()
     while (True):
@@ -20,16 +41,11 @@ if __name__=="__main__":
         response=requests.get(url)
         response.encoding=response.apparent_encoding
         data=response.json()
+        print(data)
         image=data["response"][0]['image_urls']['large']
-        if (mainland):
-            image="https://bigimg.cheerfun.dev/get/"+image
-            command="aria2c "+image+" --user-agent=\""+user_agent+"\""
-        else:
-            command="aria2c "+image+" --user-agent=\""+user_agent+"\""+" --referer=https://www.pixiv.net/artworks/"+id
-        print("Downloading "+image)
-        print("Command is:"+command)
-        os.system(command)
-
-        # command="aria2c "+image+" --user-agent=\""+user_agent+"\""+" --referer=https://www.pixiv.net/artworks/"+id
-        # command="aria2c "+image+" --conf-path=aria2.conf"+" --referer=https://www.pixiv.net/artworks/"+id
-        # command="saldl "+image+" --referer=https://www.pixiv.net/artworks/"+id+" --skip-TLS-verification --connections=15 --connection-max-rate=0 --chunk-size=4M"
+        download(image,id)
+        # fout = open(outfile,'w',encoding='utf8')
+        # fout.write(ans)
+        # fout.close()
+        # os.system("pause")
+        # 
